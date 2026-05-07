4a300d589c5d8b2d1d9cc7f84a7a1d93f75d2c4c
Jagger Adams <jadam85@uwo.ca>
2026-05-06 22:36:56 -0400

    UI improvements

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,29 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 import BarCard from '@/components/BarCard';
 import { Bar } from '@/types/Bar';
 
-const testBar: Bar = {
-  id: '1',
-  name: "Delilah's",
-  type: 'Cocktail Bar',
-  address: 'King St W',
-  busyness: 3,
-};
+const bars: Bar[] = [{
+    id: '1',
+    name: "Delilah's",
+    type: 'Cocktail Bar',
+    address: 'King St W',
+    busyness: 3,
+}, 
+{
+    id: '2',
+    name: "Barney's",
+    type: 'Dance Bar',
+    address: 'Eagle St N',
+    busyness: 1,
+  }, 
+  {
+    id: '3',
+    name: "Joe Cool's",
+    type: 'Pub',
+    address: 'King St W',
+    busyness: 2,
+}, 
+{
+    id: '4',
+    name: "The Warehouse",
+    type: 'Club',
+    address: 'Eagle St N',
+    busyness: 3,
+  }, 
+  {
+    id: '5',
+    name: "Kentucky",
+    type: 'Country Bar',
+    address: 'King St W',
+    busyness: 2,
+}, 
+{
+    id: '6',
+    name: "Pub on King",
+    type: 'Lounge',
+    address: 'King St N',
+    busyness: 1,
+  }, 
+];
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, padding: 16 }}>
-      <BarCard bar={testBar} />
-    </View>
+    <ScrollView style={styles.container} contentContainerStyle={styles.content}>
+      {bars.map((bar) => (
+        <BarCard key={bar.id} bar={bar} />
+      ))}
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#d4eb29',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#111111',
+  },
+  content: {
+    padding: 12,
+    gap: 12,
   },
 });
--- components/BarCard.tsx
+++ components/BarCard.tsx
@@ -3,18 +3,37 @@ import { Text, View, StyleSheet } from 'react-native';
 import { Bar } from '@/types/Bar';
 import scale from '@/utils/scale';
 
+const getBusynessLabel = (level: number): string => {
+    if (level == 1) return 'Chill';
+    if (level == 2) return 'Busy';
+    if (level == 3) return 'Packed';
+    return 'packed'
+};
+
+const getBusynessColor = (level: number): string => {
+    if (level == 1) return '#2a9962';
+    if (level == 2) return '#b8861a';
+    return 'red'
+};
+
+const getBusynessBackgroundColor = (level: number): string => {
+    if (level === 1) return 'rgba(78, 203, 135, 0.4)';
+    if (level === 2) return 'rgba(232, 184, 109, 0.4)';
+    return 'rgba(224, 92, 92, 0.4)';
+};
+
 const StatusCapsule = ({ level }: { level: number }) => {
   return (
-    <View style={styles.capsule}>
-      <Text style={styles.capsuleText}>{level}</Text>
+    <View style={[styles.capsule, { backgroundColor: getBusynessBackgroundColor(level)}]}>
+      <Text style={[styles.capsuleText, { color: getBusynessColor(level) }]}>{getBusynessLabel(level)}</Text>
     </View>
   );
 };
 
 const StatusBar = ({ level }: { level: number }) => {
   return (
-    <View style={{width: '95%', height: 10, backgroundColor: 'grey', borderRadius: 12}}>
-      <View style={{width: `${30 * level}%`, height: 10, backgroundColor: 'red', borderRadius: 12}}>
+    <View style={{width: '100%', height: 10, backgroundColor: 'grey', borderRadius: 12}}>
+      <View style={{width: `${30 * level}%`, height: 10, backgroundColor: getBusynessColor(level), borderRadius: 12}}>
       </View>
     </View>
   );
@@ -40,9 +59,9 @@ const styles = StyleSheet.create({
   card: {
     flexDirection: 'column',
     borderRadius: 16,
-    backgroundColor: 'green',
+    backgroundColor: '#3f3954',
     padding: 16,
-    gap: 4
+    gap: 12
   },
   row: {
     flexDirection: 'row',
@@ -51,20 +70,20 @@ const styles = StyleSheet.create({
   },
   col: {
     flexDirection: 'column',
-    gap: 2
+    gap: 4
   },
-  name: { fontSize: scale(32) },
-  sub: { fontSize: scale(16) },
+  name: { fontSize: scale(32), color: 'white' },
+  sub: { fontSize: scale(16), color: 'white' },
   capsule: {
-    paddingHorizontal: 12,
-    paddingVertical: 6,
+    paddingHorizontal: 16,
+    paddingVertical: 10,
     borderRadius: 20,
     backgroundColor: '#e8b86d',
     justifyContent: 'center',
     alignItems: 'center',
   },
   capsuleText: {
-    fontSize: scale(12),
+    fontSize: scale(24),
     fontWeight: '500',
   },
 });
